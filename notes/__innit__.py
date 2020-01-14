18e1f65ca721053913931e91e2a332e9b7e68ba8
AVData <agustinvargas@protonmail.com>
2020-01-14 13:05:46 -0800

    Created a virtual environment with pipenv, adding files

---
+++ notes/__innit__.py
@@ -0,0 +1,10 @@
+'''
+lambdata - a collection of data science helpter functions
+'''
+
+import pandas as import pd
+import numpy as np
+
+# sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))

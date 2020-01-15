54ded7896226bb2d7d08da4840f1aa9b7653c090
AVData <AVData@users.noreply.github.com>
2020-01-14 16:33:46 -0800

    cleaned up code to be pep8 compatible

--- lambdata_AVData/__init__.py
+++ lambdata_AVData/__init__.py
@@ -8,46 +8,42 @@ Assignment 1
 # ONES = pd.DataFrame(np.ones(10))
 # ZEROS = pd.DataFrame(np.zeros(50))
 
+# Write a program to read through the mbox-short.txt and figure out who has
+# sent the greatest number of mail messages.  The program looks for "From "
+# lines and takes the second word of those lines as the person who sent the
+# mail.  The program creates a Python dictionary that maps the sender's mail
+# address to a count of the number of times they appear in the file.  After the
+# dictionary is produced, the program reads through the dictionary using a
+# maximum loop to find the most prolific committer.
 
-name = input("Enter file: ")
-fhand = open(name)
-words = list()
+
+NAME = input("Enter file: ")
+FHAND = open(NAME)
+WORDS = list()
 #count = 0
 
-for line in fhand:
-    if not line.startswith("From "): continue
+for line in FHAND:
+    if not line.startswith("From "):
+        continue
     line = line.split()
-    words.append(line[1])
+    WORDS.append(line[1])
 #    emails[words] = line[1]
 
-counts = dict()
+COUNTS = dict()
 
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
+for word in WORDS:
+    COUNTS[word] = COUNTS.get(word, 0) + 1
 
-bigcount = None
-bigword = None
-for email,counts in counts.items():
-    if bigcount is None or counts > bigcount:
-        bigword = email
-        bigcount = counts
+BIGCOUNTS = None
+BIGWORD = None
+for email, COUNTS in COUNTS.items():
+    if BIGCOUNTS is None or COUNTS > BIGWORD:
+        BIGWORD = email
+        BIGCOUNTS = COUNTS
 
-print(bigword, bigcount)
+print(BIGWORD, BIGCOUNTS)
+
+
 #    count = count + 1
 #    print(words[1])
 #print("There were", count, "lines in the file with From as the first word")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#

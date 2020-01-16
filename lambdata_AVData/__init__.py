5dd0f7d2238a0697ab2c074b09e4cc31ae95c4ca
AVData <AVData@users.noreply.github.com>
2020-01-16 13:37:45 -0800

    Lecture notes for thursday were included in folder notes_thursday; files were modified and notes folder was deleted.  new dist files were created

--- lambdata_AVData/__init__.py
+++ lambdata_AVData/__init__.py
@@ -1,46 +1,30 @@
 '''
 lambdata - a  collection of data science helper funcitons
 '''
-#
-import pandas as pd
-import numpy as np
-import sklearn
-
-# Write a program to read through the mbox-short.txt and figure out who has
-# sent the greatest number of mail messages.  The program looks for "From "
-# lines and takes the second word of those lines as the person who sent the
-# mail.  The program creates a Python dictionary that maps the sender's mail
-# address to a count of the number of times they appear in the file.  After the
-# dictionary is produced, the program reads through the dictionary using a
-# maximum loop to find the most prolific committer.
 
 
-# name = input("Enter file: ")
-# fhand = open(name)
-# words = list()
-# #count = 0
-#
-# for line in fhand:
-#     if not line.startswith("From "): continue
-#     line = line.split()
-#     words.append(line[1])
-# #    emails[words] = line[1]
-#
-# counts = dict()
-#
-# for word in words:
-#     counts[word] = counts.get(word, 0) + 1
-#
-# bigcount = None
-# bigword = None
-# for email,counts in counts.items():
-#     if bigcount is None or counts > bigcount:
-#         bigword = email
-#         bigcount = counts
-#
-# print(bigword, bigcount)
+def mean(x):
+    return (sum(x)/len(x))
+    print(mean(x))
+
+def median(x):
+    sortedx = sorted(x)
+    xlen = len(x)
+    index = (xlen - 1) // 2
+
+    if (xlen % 2):
+        return sortedx[index]
+    else:
+        return (sortedx[index] + sortedx[index + 1])/2.0
+
+def mode(x):
+    most = max(list(map(x.count, x)))
+    return list(set(filter(lambda y: x.count(y) == most, x)))
+
+def meanMedianMode(numbers):
+    return print('Mean', mean(numbers), '\n',
+    'Median', median(numbers), '\n',
+    'Mode', mode(numbers))
 
 
-#    count = count + 1
-#    print(words[1])
-#print("There were", count, "lines in the file with From as the first word")
+# writing a fucntino for thursday

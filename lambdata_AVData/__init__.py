ae457f278bf26f0a38f5b14bba3b3ddc944da1df
Agustin Vargas <50128461+AVData@users.noreply.github.com>
2020-06-30 14:21:09 -0700

    updated the file to contain better documentation.

--- lambdata_AVData/__init__.py
+++ lambdata_AVData/__init__.py
@@ -1,30 +1,46 @@
 '''
-lambdata - a  collection of data science helper funcitons
+A  collection of data science funcitons.
+
+Mean, Median, and Mode are three kinds of statistical averages.
+In the following I will be utilizing the python coding language to implement
+three functions that take in a series of values and return either the Mean,
+Median, or Mode, or all three.
 '''
 
 
 def mean(x):
-    return (sum(x)/len(x))
-    print(mean(x))
+    '''
+    The mean for a given set of values is the sum of all of the values, devided
+    by the number of values.
+    '''
+    mean_output = sum(x)/len(x)
+    return mean_output
+
 
 def median(x):
-    sortedx = sorted(x)
-    xlen = len(x)
-    index = (xlen - 1) // 2
+    '''
+    The median for a given set of values is the middle most value in sorted
+    list of values.
+    '''
+    sorted_x = sorted(x)
+    x_len = len(x)
+    index = (x_len - 1) // 2
 
-    if (xlen % 2):
-        return sortedx[index]
+    if (x_len % 2):
+        return sorted_x[index]
     else:
-        return (sortedx[index] + sortedx[index + 1])/2.0
+        return (sorted_x[index] + sorted_x[index + 1])/2.0
+
 
 def mode(x):
+    '''
+    The mode for a given set of values is the value that appears the most
+    frequently in a given list of values.
+    '''
     most = max(list(map(x.count, x)))
     return list(set(filter(lambda y: x.count(y) == most, x)))
 
+
 def meanMedianMode(numbers):
-    return print('Mean', mean(numbers), '\n',
-    'Median', median(numbers), '\n',
-    'Mode', mode(numbers))
-
-
-# writing a fucntino for thursday
+    return ('Mean', mean(numbers), '\n', 'Median', median(numbers), '\n',
+            'Mode', mode(numbers))
